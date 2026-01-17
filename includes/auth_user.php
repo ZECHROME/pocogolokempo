9118b2045893f406bf8d6ecde86582c317bc5532
Kenny Kora <kennethkoraag05@gmail.com>
2026-01-18 01:19:20 +0800

    auth user

--- includes/auth_user.php
+++ includes/auth_user.php
@@ -1,4 +1,9 @@
 <?php
-if (!isset($_SESSION['admin'])) {
-    die("Access denied");
+if (!isset($_SESSION['user_id'])) {
+    header("Location: /pocogolo/auth/login.php");
+    exit;
+}
+
+if (!$_SESSION['email_verified']) {
+    exit("Please verify your email before accessing documents.");
 }
