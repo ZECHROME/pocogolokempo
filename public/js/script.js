5f14441c28fa870c955a56364e87d0acd68e6576
Kenny Kora <kennethkoraag05@gmail.com>
2026-04-23 05:19:55 +0800

    showRegister showLogin

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
+function showRegister() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "block";
+}
+
+function showLogin() {
+    document.getElementById("registerForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "block";
+}
+
 function initStars() {
   const canvas = document.getElementById("stars-canvas");
   if (!canvas) return;
